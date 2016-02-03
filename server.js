c4851b1a299ae6ec4e527e60eec635ae857ae163
Jess Johnson <jessica.irish.johnson@gmail.com>
2016-02-03 23:18:49 +0000

    commenting out more..

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 'use strict';
 
 var express = require('express');
+/*
 var routes = require('./app/routes/index.js');
 var mongoose = require('mongoose');
 var passport = require('passport');
 var session = require('express-session');
+*/
 
 var app = express();
 require('dotenv').load();
